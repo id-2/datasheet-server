96558a0cf7982fa9ffea1aafa3cf8250c6636bec
msramalho <19508417+msramalho@users.noreply.github.com>
2022-02-16 12:26:37 +0100

    updating example config

--- src/example.config.js
+++ src/example.config.js
@@ -8,5 +8,12 @@ export default {
       path: 'data/timemap_data.xlsx',
       tabs: timemap.default
     }
-  ]
+  ],
+  cors: {
+    active: false,
+    corsOptions: { // meaningless unless active=true
+      origin: 'http://example.com',
+      optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    }
+  }
 }
